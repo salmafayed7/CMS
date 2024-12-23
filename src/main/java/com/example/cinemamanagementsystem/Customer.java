8b21468b46320bdfa2c83f58ad662c1b988e1f9f
salmafayed7 <s.fayed00870@student.aast.edu>
2024-12-23 21:37:43 +0200

    yarab

--- src/main/java/com/example/cinemamanagementsystem/Customer.java
+++ src/main/java/com/example/cinemamanagementsystem/Customer.java
@@ -2,5 +2,15 @@ package com.example.cinemamanagementsystem;
 import java.util.*;
 public class Customer extends Person {
     public int customerID;
-    public
+    public ArrayList<Booking> bookingHistory;
+    public int points;
+
+    public Customer(String name, String email, String password, int phone) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.customerID = phone;
+        bookingHistory = new ArrayList<Booking>();
+    }
 }
