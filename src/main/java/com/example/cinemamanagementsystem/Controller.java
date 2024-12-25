a7e6df06acd99630853c0fe38fbcc808a8bfa8ea
farida <faridakhaled05@gamil.com>
2024-12-25 20:47:25 +0200

    db updates

---
+++ src/main/java/com/example/cinemamanagementsystem/Controller.java
@@ -0,0 +1,55 @@
+package com.example.cinemamanagementsystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+public abstract class Controller {
+
+
+        public void switchScene(ActionEvent event, String fxmlFile, String title, Stage stage, Scene scene) throws IOException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+            scene=new Scene(root,621, 498);
+            stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle(title);
+        }
+        public void infoBox(String infoMessage, String headerText, String title) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setContentText(infoMessage);
+            alert.setTitle(title);
+            alert.setHeaderText(headerText);
+            alert.showAndWait();
+        }
+
+
+        public void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+            Alert alert = new Alert(alertType);
+            alert.setTitle(title);
+            alert.setHeaderText(null);
+            alert.setContentText(message);
+            alert.initOwner(owner);
+            alert.show();
+        }
+        public void showAlert(Window owner, String message) {
+            showAlert(Alert.AlertType.INFORMATION, owner, "Success", message);
+        }
+        public void showAlert(Window owner) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Form Error!");
+            alert.setHeaderText(null);
+            alert.setContentText("Text field empty");
+            alert.initOwner(owner);
+            alert.show();
+        }
+    }
+
+
